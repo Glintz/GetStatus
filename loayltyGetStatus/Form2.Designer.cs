36742adb8453f7719ac5ea22f4822ab845874d74
Paul Stewart <pstewart@mercurypay.com>
2013-04-08 09:21:26 -0600

    GetStatus Demo

---
+++ loayltyGetStatus/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace loayltyGetStatus
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Getstatus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "AddCredits";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(585, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Result";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(306, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(394, 91);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 206);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(474, 219);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(588, 206);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(440, 232);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Request";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 466);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Loyalty using RESTful";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ loayltyGetStatus/Form2.cs
@@ -0,0 +1,151 @@
+﻿using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System;
+
+
+namespace loayltyGetStatus
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //A Basic GetStatus()
+                string url = "http://api.cert.mercuryloyalty.com/v3_0/loyalty/GetStatus/?apikey=PAUL&apisecret=f2ee0e69a14c45039c6041b19fd18d88&customeridentifier=4076374996";
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "text/xml";
+                httpWebRequest.Method = WebRequestMethods.Http.Get;
+                var response = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader responseReader = new StreamReader(responseStream);
+                string responseString = responseReader.ReadToEnd();
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(responseString);
+                //* Get elements.
+                string emailAddress = xmlDoc.GetElementsByTagName("EmailAddress")[0].InnerText;
+                string firstName = xmlDoc.GetElementsByTagName("FirstName")[0].InnerText;
+                string lastName = xmlDoc.GetElementsByTagName("LastName")[0].InnerText;
+                string mobileNumber = xmlDoc.GetElementsByTagName("MobileNumber")[0].InnerText;
+                string currentCredits = xmlDoc.GetElementsByTagName("CurrentCredits")[0].InnerText;
+                string lifeTimeCredits = xmlDoc.GetElementsByTagName("LifetimeCredits")[0].InnerText;
+                string lifeTimeRevenue = xmlDoc.GetElementsByTagName("LifetimeRevenue")[0].InnerText;
+                string rewardsEarned = xmlDoc.GetElementsByTagName("RewardsEarned")[0].InnerText;
+
+
+                textBox1.Text = "";
+                textBox1.AppendText("Email: " + emailAddress);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("First Name: " + firstName);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("Last Name: " + lastName);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("Mobile Number: " + mobileNumber);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("Current Credits: " + currentCredits);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("Lifetime Credits: " + lifeTimeCredits);
+                textBox1.AppendText(Environment.NewLine);
+
+
+                textBox1.AppendText("Lifetime Revenue: " + lifeTimeRevenue);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox1.AppendText("Rewards Earned: " + rewardsEarned);
+                textBox1.AppendText(Environment.NewLine);
+
+                textBox3.Text = url;
+
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "";
+                textBox1.AppendText("An Error has Occured: ");
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText(ex.Message);
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string url2 = "https://api.cert.mercuryloyalty.com/v3_0/loyalty/AddCredits/?apikey=PAUL&apisecret=f2ee0e69a14c45039c6041b19fd18d88&units=10&description=AddCredits&employee_id=1&station_id=1&ticket_id=2012-10-18-10-54-44-101&revenue=10&client=MERCURY&version=1.0.3.822&customeridentifier=4075554997";
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url2);
+                httpWebRequest.Method = WebRequestMethods.Http.Get;
+                var response = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader responseReader = new StreamReader(responseStream);
+                string responseString = responseReader.ReadToEnd();
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml("<root>" + responseString + "</root>");
+                XmlNodeList address = xmlDoc.GetElementsByTagName("original");
+                textBox1.Text = responseString;
+                textBox3.Text = url2;
+            }
+            catch (Exception ex)
+            {
+
+
+                textBox1.Text = "";
+                textBox1.AppendText("An Error has Occured: ");
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText(ex.Message);
+                textBox1.AppendText(Environment.NewLine);
+
+
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
